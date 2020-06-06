59bb3edabcfe4fbf90e30e3f3ab85e9cc25a639b
Christian Tischer <mail@tischi.de>
2020-06-06 21:32:31 +0200

    Added missing refresh interval

--- hddexporter.go
+++ hddexporter.go
@@ -27,30 +27,33 @@ func main() {
 	http.Handle("/metrics", promhttp.Handler())
 	prometheus.MustRegister(temperatureGauge)
 	go func() {
-		conn, err := net.DialTimeout("tcp", "localhost:7634", time.Second*3)
-		if err != nil {
-			log.Fatal(err)
-		}
-		defer conn.Close()
-
-		data, err := ioutil.ReadAll(conn)
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		fields := strings.Split(string(data), "|")
-		for index := 0; index < len(fields)/5; index++ {
-			offset := index * 5
-			device := fields[offset+1]
-			device = device[strings.LastIndex(device, "/")+1:]
-			temperatureField := fields[offset+3]
-			temperature, err := strconv.ParseFloat(temperatureField, 64)
+		for {
+			conn, err := net.DialTimeout("tcp", "localhost:7634", time.Second*3)
 			if err != nil {
-				fmt.Println(err)
-				continue
+				log.Fatal(err)
 			}
-			temperatureGauge.WithLabelValues(device).Set(temperature)
-	}
+			defer conn.Close()
+
+			data, err := ioutil.ReadAll(conn)
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			fields := strings.Split(string(data), "|")
+			for index := 0; index < len(fields)/5; index++ {
+				offset := index * 5
+				device := fields[offset+1]
+				device = device[strings.LastIndex(device, "/")+1:]
+				temperatureField := fields[offset+3]
+				temperature, err := strconv.ParseFloat(temperatureField, 64)
+				if err != nil {
+					fmt.Println(err)
+					continue
+				}
+				temperatureGauge.WithLabelValues(device).Set(temperature)
+			}
+			time.Sleep(time.Second * 10)
+     		}
 	}()
 
 	log.Fatal(http.ListenAndServe(":8080", nil))
